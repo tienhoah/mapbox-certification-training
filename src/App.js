cee35dd9986e161e97e32c1b98e5042a96f30572
Nick Hoang <nickhoang11@gmail.com>
2020-08-25 18:12:08 -0700

    Add bus 99 translink tracking

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Mapbox from "./components/Mapbox";
 function App() {
   return (
     <>
-      <h1>Hello</h1>
       <Mapbox />
     </>
   );
--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import translink_routes from "../apis/translink_routes";
+// import Marker from "./Marker";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const Mapbox = () => {
-  return (
-    <>
-      <h2>Map</h2>
-    </>
-  );
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/nickhoang11/ckeaaht6x03hz19plpnhqkg6h",
+      center: [-123.12, 49.26427],
+      zoom: 12,
+    });
+
+    map.on("load", () => {
+      window.setInterval(() => {
+        translink_routes.get().then((res) => {
+          const busLocation = {
+            type: "FeatureCollection",
+            features: [],
+          };
+          res.data.forEach((bus) => {
+            const busLatLng = {
+              type: "Point",
+              coordinates: [bus.Longitude, bus.Latitude],
+            };
+            const markerPoint = { type: "Feature", geometry: busLatLng };
+
+            busLocation.features.push(markerPoint);
+          });
+
+          if (map.getSource("bus99") !== undefined) {
+            map.getSource("bus99").setData(busLocation);
+          }
+        });
+      }, 3000);
+
+      map.addSource("bus99", {
+        type: "geojson",
+        data: { type: "FeatureCollection", features: [] },
+      });
+
+      map.addLayer({
+        id: "bus",
+        type: "symbol",
+        source: "bus99",
+        layout: {
+          "icon-image": "bus",
+        },
+      });
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return <div className="map-container" ref={mapContainerRef} />;
 };
 
 export default Mapbox;
